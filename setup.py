d77142de491820b1959465b6addcf788d969f7f9
Brendan McCollam <bmccollam@leapfrogonline.com>
2011-10-31 14:21:35 -0500

    Bumping version number to reflect new xeger feature

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rstr',
-      version='1.0',
+      version='2.0',
       description='Generate random strings in Python',
       author='Leapfrog Direct Response LLC',
       author_email='oss@leapfrogdevelopment.com',
