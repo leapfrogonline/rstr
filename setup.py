b7e0cf8f63c4a9691897d26f185b54e6985842d2
Brendan McCollam <brendan@mccoll.am>
2013-06-07 11:34:43 -0500

    Cleanup and version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import unittest
 
 setup(name='rstr',
-      version='2.1.0',
+      version='2.1.1',
       description='Generate random strings in Python',
       author='Leapfrog Direct Response LLC',
       author_email='oss@leapfrogdevelopment.com',
