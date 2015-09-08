93f7227a8c5d5beec2480b8e746c3ffc18ecae4d
Brendan McCollam <brendan@mccoll.am>
2015-09-07 19:13:59 -0500

    Bump version, PEP8-ifying, minor bugfix

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
-import unittest
 
 setup(name='rstr',
-      version='2.1.3',
+      version='2.2.0',
       description='Generate random strings in Python',
       author='Leapfrog Direct Response LLC',
       author_email='oss@leapfrogdevelopment.com',
       url='http://bitbucket.org/leapfrogdevelopment/rstr/overview',
       packages=['rstr'],
       test_suite='rstr.tests.suite',
-     )
+      )
